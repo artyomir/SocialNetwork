46ab8078e42c19a09c9f3a3129f6cda96136672a
artyomir <artyomsonyx@gmail.com>
2020-08-20 14:54:44 +0300

    Implemented react-router

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
+import { Route, BrowserRouter } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div className="app-wrapper">
-      <Header></Header>
-      <Navbar></Navbar>
-      <div className="app-wrapper-content">
-        <Profile />
-        {/* <Dialogs /> */}
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <Navbar />
+        <div className="app-wrapper-content">
+          <Route path="/messages" component={Dialogs} />
+          <Route path="/profile" component={Profile} />
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 };
 
